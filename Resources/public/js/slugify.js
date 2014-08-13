18bc8f7ed33e07c10e975b0b06be532a984832f8
proszek <proszek@iMac-proszek.local>
2014-08-13 10:12:34 +0200

    Moved jstree resources to the admin bundle.

---
+++ Resources/public/js/slugify.js
@@ -0,0 +1,9 @@
+if ($('#btn_nodesbundle_nodetype_title').length) {
+    $('#btn_nodesbundle_nodetype_slug').slugify('#btn_nodesbundle_nodetype_title');
+};
+
+//init plugin
+// $('#nodesManager').nodesTreeManager();
+
+//confirmations
+// $('a[data-confirm]').confirmModal();
