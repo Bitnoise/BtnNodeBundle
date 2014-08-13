3a433833cea045211b3668f1d068e010dad13048
proszek <proszek@iMac-proszek.local>
2014-08-13 16:24:12 +0200

    Moved bind #tree to jstree to admin bundle, handle change.jstree event only in the nodes bundle.

--- Resources/public/js/nodes.js
+++ Resources/public/js/nodes.js
@@ -1,3 +1,11 @@
+$('#tree')// listen for event
+    .on('changed.jstree', function (e, data) {
+        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#') {
+            window.location = data.node.a_attr.href;
+        };
+    })
+;
+
 if ($('#btn_nodesbundle_nodetype_title').length) {
     $('#btn_nodesbundle_nodetype_slug').slugify('#btn_nodesbundle_nodetype_title');
 };
