6789945945f48231f3aff828ae02d0638da45cc0
proszek <proszek@iMac-proszek.local>
2014-08-14 10:47:17 +0200

    Moved on change event ot the admin bundle.

--- Resources/public/js/slugify.js
+++ Resources/public/js/slugify.js
@@ -1,11 +1,3 @@
-$('#tree')// listen for event
-    .on('changed.jstree', function (e, data) {
-        if (typeof data.node !== 'undefined' && data.node.a_attr.href !== '#') {
-            window.location = data.node.a_attr.href;
-        };
-    })
-;
-
 if ($('#btn_nodesbundle_nodetype_title').length) {
     $('#btn_nodesbundle_nodetype_slug').slugify('#btn_nodesbundle_nodetype_title');
 };
