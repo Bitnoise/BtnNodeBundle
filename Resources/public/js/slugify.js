b64931ab38946eb6693c5d7c591ebe2cde588b95
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-18 22:46:40 +0200

    remove old assets

--- Resources/public/js/slugify.js
+++
@@ -1,9 +0,0 @@
-if ($('#btn_node_nodetype_title').length) {
-    $('#btn_node_nodetype_slug').slugify('#btn_node_nodetype_title');
-};
-
-//init plugin
-// $('#nodesManager').nodesTreeManager();
-
-//confirmations
-// $('a[data-confirm]').confirmModal();
