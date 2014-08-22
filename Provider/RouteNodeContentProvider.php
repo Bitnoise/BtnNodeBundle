2511200ef5390dcbba2324011c3da00bdaf46858
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-22 10:10:10 +0200

    rename to RoteNodeContentProvider

--- Provider/RouteNodeContentProvider.php
+++ Provider/RouteNodeContentProvider.php
@@ -4,47 +4,64 @@ namespace Btn\NodeBundle\Provider;
 
 use Btn\NodeBundle\Form\RouteContentType;
 
-/**
-*
-*
-*/
-class RouteContentProvider implements NodeContentProviderInterface
+class RouteNodeContentProvider implements NodeContentProviderInterface
 {
+    /** @var array $availableRoutes */
+    protected $availableRoutes = array();
 
-    private $availableRoutes = array();
-
+    /**
+     *
+     */
     public function __construct($availableRoutes)
     {
         $this->availableRoutes = $availableRoutes;
     }
 
+    /**
+     *
+     */
     public function getForm()
     {
         return new RouteContentType($this->availableRoutes);
     }
 
+    /**
+     *
+     */
     public function resolveRoute($formData = array())
     {
         return $formData['route'];
     }
 
+    /**
+     *
+     */
     public function resolveRouteParameters($formData = array())
     {
         return array();
     }
 
+    /**
+     *
+     */
     public function resolveControlRoute($formData = array())
     {
         return null;
     }
 
+    /**
+     *
+     */
     public function resolveControlRouteParameters($formData = array())
     {
         return array();
     }
 
+    /**
+     *
+     */
     public function getName()
     {
-        return 'Internal routes';
+        return 'btn_node.route_node_content_provider.name';
     }
 }
