50a00627ab6cade31a925510b04837af1da593ce
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-25 14:29:08 +0200

    add node events descriptions

--- NodeEvents.php
+++ NodeEvents.php
@@ -4,9 +4,24 @@ namespace Btn\NodeBundle;
 
 final class NodeEvents
 {
+    /**
+     * Event is triggerd when new node was created
+     */
     const NODE_CREATED      = 'btn_node.node_created';
+    /**
+     * Event is triggerd when node was updated
+     */
     const NODE_UPDATED      = 'btn_node.node_updated';
+    /**
+     * Event is triggerd when node was deleted
+     */
     const NODE_DELETED      = 'btn_node.node_deleted';
+    /**
+     * Event is triggerd when node provider was changed
+     */
     const PROVIDER_CHANGED  = 'btn_node.provider_changed';
+    /**
+     * Event is triggerd when node provider parameters were modified
+     */
     const PROVIDER_MODIFIED = 'btn_node.provider_modified';
 }
