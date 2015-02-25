7c33033fbdb88f46ab6fbf9a607d2d1562d6c357
Adam Misiorny <adam.misiorny@gmail.com>
2015-02-25 16:34:06 +0100

    update btn_node based on select2

--- src/Btn/NodeBundle/Form/Type/NodeType.php
+++ src/Btn/NodeBundle/Form/Type/NodeType.php
@@ -8,33 +8,48 @@ use Doctrine\ORM\EntityRepository;
 
 class NodeType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         parent::setDefaultOptions($resolver);
 
         $resolver->setDefaults(array(
-            'empty_value'   => 'btn_node.type.node.empty_value',
-            'label'         => 'btn_node.type.node.label',
-            'class'         => $this->entityProvider->getClass(),
-            'attr'          => array(
+            'empty_value' => 'btn_node.type.node.empty_value',
+            'label'       => 'btn_node.type.node.label',
+            'class'       => $this->entityProvider->getClass(),
+            'attr'        => array(
                 'class' => 'btn-node',
             ),
             'query_builder' => function (EntityRepository $em) {
                 return $em
-                    ->createQueryBuilder('node')
-                    ->orderBy('node.title', 'ASC')
+                    ->createQueryBuilder('n')
+                    ->leftJoin('n.rootEntity', 're')
+                    ->select('n, re')
+                    ->andWhere('n.lvl > 0')
+                    ->orderBy('n.root', 'ASC')
+                    ->addOrderBy('n.lft', 'ASC')
                 ;
             },
+            'group_by' => 'rootEntity.name',
+            'property' => 'titleLvlPrefixed',
             'required' => true,
             'expanded' => false,
         ));
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getParent()
     {
-        return 'entity';
+        return 'btn_select2_entity';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'btn_node';
